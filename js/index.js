baa0a1bbaebbf8f0e9c5f83e733b62b0e88eb8fb
hujunzheng <junzheng.hu@ele.com>
2018-03-29 02:16:16 +0800

    调整代码结构

--- js/index.js
+++ js/index.js
@@ -6,6 +6,16 @@ function ballFrameOnload(){
 //    $('#more-content').height($('#ball-frame').contents().find("body").height());
 }
 
+function adjustProject(projectDiv, leftDiv, rightDiv) {
+    projectDiv.each(function(index){
+        if(((index+1)&1) == 0) {
+            rightDiv.append($(this));
+        } else {
+            leftDiv.append($(this));
+        }
+    });
+}
+
 function project() {
 	$('#my-project>div>div').replaceWith(function(index){
 		/*
@@ -29,26 +39,22 @@ function project() {
 	
 	//加载 p-div中的内容
 	$('#my-project .p-div .content').each(function(index){
-		$.ajax( {
-		    url: 'resume/../project_template/project' + index + '.html', //这里是静态页的地址
-		    type: "GET", //静态页用get方法，否则服务器会抛出405错误
-		    success: (data)=>{$(this).html(data);},
-		    error: (data)=>{$(this).html('<p>加载错误!!!</p>');}
-		});
+		// $.ajax( {
+		//     url: 'resume/../project_template/project' + index + '.html', //这里是静态页的地址
+		//     type: "GET", //静态页用get方法，否则服务器会抛出405错误
+		//     success: (data)=>{$(this).html(data);},
+		//     error: (data)=>{$(this).html('<p>加载错误!!!</p>');}
+		// });
 	});
-	
+
+    var myProjectDivH = $('#my-project').height;
 	//将p-div中的内容放到 left-div（奇数） 和 right-div(偶数)
 	var leftDiv = $('<div class="left-div"></div>');
 	var rightDiv = $('<div class="right-div"></div>');
 	var projectDiv = $($.makeArray($('#my-project .p-div')).reverse());
-	
-	projectDiv.each(function(index){
-		if(((index+1)&1) == 0) {
-			rightDiv.append($(this));
-		} else {
-			leftDiv.append($(this));
-		}
-	});
+
+    adjustProject(projectDiv, leftDiv, rightDiv);
+
 	$('#my-project').empty().append(leftDiv).append(rightDiv);
 	
 	$('#my-project .slideOutTip').each(function(){
@@ -70,11 +76,32 @@ function project() {
 		if(tip.is(':animated'))
 			return false;
 
-		if(tip.find('.slideOutContent').css('display') == 'none')
-		{
+		if(tip.find('.slideOutContent').css('display') == 'none') {
+            var pdiv = tip.parent(), delDiv, addDiv;
+
+            if(pdiv.parent().is(leftDiv)) {
+                delDiv = leftDiv;
+                addDiv = rightDiv;
+            } else {
+                delDiv = rightDiv;
+                addDiv = leftDiv;
+            }
+            delDiv.children().each(function(){
+                var curPdiv = $(this);
+                if(!curPdiv.is(pdiv)) {
+                    curPdiv.detach();
+                    addDiv.append(curPdiv);
+                }
+            });
 			tip.trigger('slideOut');
 		}
-		else tip.trigger('slideIn');
+		else {
+            projectDiv.each(function () {
+                $(this).detach();
+            })
+            adjustProject(projectDiv, leftDiv, rightDiv);
+            tip.trigger('slideIn');
+        }
 
 	});
 	
@@ -139,13 +166,17 @@ function project() {
 		 * Resize the slideout to fit the content, which is then faded into
 		 * view:
 		 */
-		
+        var slideOutTipH = slideOut.outerHeight() + tip.data('origHeight');
 		tip.addClass('isOpened').animate({
 			width	: Math.max(slideOut.outerWidth(),tip.data('origWidth')),
-			height	: slideOut.outerHeight()+tip.data('origHeight')
+			height	: slideOutTipH
 		},function(){
 			slideOut.fadeIn();
 		});
+
+		if(slideOutTipH > myProjectDivH) {
+            $('#my-project').animate({height: slideOutTipH});
+        }
 
 	}).bind('slideIn',function(){
 		var tip = $(this);
@@ -159,13 +190,14 @@ function project() {
 			},function(){
 				tip.removeClass('isOpened');
 			});
+            $('#my-project').animate({height: myProjectDivH});
 		});
 
 	});
 }
 
 $(function () {
-	//头像
+    //头像
     $(".message_head_content").mouseover(function () {
         $(this).find("#head_one").stop().animate({opacity: 0}, 300);
         $(this).find("#head_two").stop().animate({opacity: 1}, 300);
@@ -175,22 +207,6 @@ $(function () {
     });
 
     //专业技能
-
-    // $(".skill_icon").click(function(){
-    //     $(".skill_int").each(function(){
-    //         if($(this).is(":visible")){
-    //             $(this).slideUp(200);
-    //             $(this).prev().removeClass("skill_flag_scale");
-    //         }
-    //     });
-    //     if($(this).siblings(".skill_int").is(":hidden")){
-    //         $(this).siblings(".skill_int").slideDown(400);
-    //         $(this).siblings(".skill_flag").addClass("skill_flag_scale");
-    //     }else{
-    //         $(this).siblings(".skill_int").slideUp(200);
-    //         $(this).siblings(".skill_flag").removeClass("skill_flag_scale");
-    //     }
-    // });
     var skill_mastery = [90, 80, 75, 85, 85];
     $(".skill_icon").click(function () {
         if ($(this).siblings(".skill_int").is(":visible")) {
@@ -240,7 +256,7 @@ $(function () {
         var $this = $(this);
         var index = $this.index();
         $this.addClass('active').siblings().removeClass('active');
-        $('body').animate({
+        $('html,body').animate({
             scrollTop: $sections.eq(index).offset().top
         }, 300, function () {
             $window.bind('scroll', scrollHandler);
@@ -248,7 +264,7 @@ $(function () {
     });
     //滚动监听
     var scrollHandler = function () {
-        var scrollTop = $('body').scrollTop();
+        var scrollTop = $('html,body').scrollTop();
         $sections.each(function (index, item) {
             var $item = $(item);
             var top = $item.offset().top;
